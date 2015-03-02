3463f58bfc5b1fc2f9e1779cdb7c8691212c4a55
johnnymerayo <johnnymerayo@gmail.com>
2015-03-02 15:43:07 +0000

    Fix Knight movement

--- src/uk/tests/trycatch/model/Knight.java
+++ src/uk/tests/trycatch/model/Knight.java
@@ -23,8 +23,12 @@ public class Knight extends Piece {
 
 	public Boolean isTreatening(Piece p){
 		
-		// The piece is being threatened by the Knight if the piece stays in any square in a horizontal, vertical, or diagonal direction.
-		if( Math.abs(Math.abs(this.getRow() - p.getRow()) - Math.abs(this.getCol() - p.getCol())) == 1) 
+		// The piece is being threatened by the Knight if the piece stays in any square nearest square not on the same rank, file, or diagonal.
+		if( Math.abs(this.getRow() - p.getRow()) <= 2 && Math.abs(this.getCol() - p.getCol()) <= 2 // Limit a 5x5 area 
+				&&   this.getRow() != p.getRow() // Not same horizontal
+						&& this.getCol() != p.getCol() // Not same vertical      
+						&& (Math.abs(this.getRow() - p.getRow()) != Math.abs(this.getCol() - p.getCol())) // Not same diagonal
+				) 
 			return new Boolean(true);
 		
 		return new Boolean(false);
